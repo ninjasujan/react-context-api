7ab2b44db3b70aa80753d87622706a7915d19b2e
Sujan Poojarty <sujan.poojary@diamondpick.com>
2023-04-29 15:38:42 +0530

    Added MUI to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import React from "react"
-import CounterContextProvider from "./Context/CounterContext"
-
-/** Component */
-import Counter from "./Component/Counter"
-import ShowCounter from "./Component/ShowCounter"
-import CheckCounter from "./Component/CheckCounter"
-
 const App = () => {
   return (
-    <CounterContextProvider>
-      <React.Fragment>
-        <h1>React Context API</h1>
-        <img
-          src="/images/react_public.png"
-          width="200"
-          height="200"
-          alt="React Logo Public"
-        />
-
-        <Counter />
-        <ShowCounter />
-        <CheckCounter />
-      </React.Fragment>
-    </CounterContextProvider>
+    <div>
+      <h2>App Element</h2>
+    </div>
   )
 }
 
