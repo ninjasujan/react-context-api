a7555410676917730c0fd7d7243c9e3e4f3f3b03
Sujan Poojarty <sujan.poojary@diamondpick.com>
2023-05-04 14:53:37 +0530

    react webpack setup added

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
 import React from "react"
-import CounterContextProvider from "./Context/CounterContext"
-
-/** Component */
-import Counter from "./Component/Counter"
-import ShowCounter from "./Component/ShowCounter"
-import CheckCounter from "./Component/CheckCounter"
 
 const App = () => {
-  return (
-    <CounterContextProvider>
-      <React.Fragment>
-        <h1>React Context API</h1>
-        <img
-          src="/images/react_public.png"
-          width="200"
-          height="200"
-          alt="React Logo Public"
-        />
-
-        <Counter />
-        <ShowCounter />
-        <CheckCounter />
-      </React.Fragment>
-    </CounterContextProvider>
-  )
+  return <div>App.js</div>
 }
 
 export default App
